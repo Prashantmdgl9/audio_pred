5300b5d37eea07919855050b0545d3080b2d5886
Prashant Mudgal <Prashantmdgl9@users.noreply.github.com>
2024-04-01 22:47:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def zcr(data,frame_length,hop_length):
     zcr=librosa.feature.zero_crossing_rate(data,frame_length=frame_length,hop_length=hop_length)
     return np.squeeze(zcr)
 def rmse(data,frame_length,hop_length):
-    frame_length=2048,hop_length=512
+    #frame_length=2048,hop_length=512
     rmse=librosa.feature.rms(data)
     return np.squeeze(rmse)
 def mfcc(data,sr,frame_length,hop_length,flatten:bool=True):
